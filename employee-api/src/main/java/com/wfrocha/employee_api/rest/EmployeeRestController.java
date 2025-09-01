e0d06e32636bd36147018548521d27e7e52958a6
Waldey <waldeypereira@gmail.com>
2025-09-01 16:15:55 +0200

    ✨ feat(employee): add PATCH support using ObjectNode for partial updates

--- employee-api/src/main/java/com/wfrocha/employee_api/rest/EmployeeRestController.java
+++ employee-api/src/main/java/com/wfrocha/employee_api/rest/EmployeeRestController.java
@@ -1,21 +1,29 @@
 package com.wfrocha.employee_api.rest;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.wfrocha.employee_api.entity.Employee;
 import com.wfrocha.employee_api.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
+import static org.apache.tomcat.jni.SSLConf.apply;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
+    private ObjectMapper objectMapper;
+
     private EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeService employeeService) {
+    public EmployeeRestController(EmployeeService employeeService, ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
         this.employeeService = employeeService;
     }
 
@@ -45,4 +53,31 @@ public class EmployeeRestController {
         Employee updatedEmployee = employeeService.update(theEmployee);
         return updatedEmployee;
     }
+
+    @PatchMapping("/employees/{id}")
+    public Employee patchEmployee(@PathVariable int id, @RequestBody ObjectNode updates) {
+        Employee theEmployee = employeeService.findById(id);
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + id);
+        }
+
+        if (updates.has("id")) {
+            throw new RuntimeException("Employee id not allowed in request body " + id);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Converte o Employee existente para ObjectNode
+        ObjectNode existingNode = mapper.valueToTree(theEmployee);
+
+        // Mescla os updates no nó existente
+        existingNode.setAll(updates);
+
+        // Converte de volta para Employee
+        Employee patchedEmployee = mapper.convertValue(existingNode, Employee.class);
+        employeeService.save(patchedEmployee);
+        return patchedEmployee;
+    }
+
+
 }
