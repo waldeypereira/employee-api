eb0fa7faadd02303e3c04381077619936098a092
Waldey <waldeypereira@gmail.com>
2025-09-01 05:39:02 +0200

    :zap: feat: add REST controller with /employees GET endpoint

---
+++ employee-api/src/main/java/com/wfrocha/employee_api/rest/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package com.wfrocha.employee_api.rest;
+
+import com.wfrocha.employee_api.dao.EmployeeDAO;
+import com.wfrocha.employee_api.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeeDAO.findAll();
+    }
+}
